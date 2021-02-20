20f6e899adf75f1aeffd175ab83ede32fbcb2ea5
stebanmd <steban.md@gmail.com>
2021-02-20 15:47:12 -0300

    aula 05

---
+++ src/components/foundation/Text/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled, {css}  from 'styled-components'
+import PropTypes from 'prop-types'
+
+export const TextStyleVariantsMap = {
+  paragraph1: css`
+    font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
+  `,
+  smallestException: css`
+    font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
+  `,
+}
+
+
+const TextBase = styled.span`
+  ${({ variant }) => TextStyleVariantsMap[variant]}
+`
+
+export function Text({ tag, variant, children }) {
+  return (
+    <TextBase
+      variant={variant}
+      as={tag}
+    >
+      {children}
+    </TextBase>
+  );
+}
+
+Text.propTypes = {
+  tag: PropTypes.string.isRequired,
+  variant: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+}
+
+Text.defaultProps = {
+  tag: 'span',
+  variant: 'paragraph1',
+}
