56ef06003e30e0976aeea59e543a41941c1b5531
stebanmd <steban.md@gmail.com>
2021-04-02 14:38:57 -0300

    feat: test TextField component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     'cypress/globals': true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:cypress/recommended',
@@ -25,4 +26,17 @@ module.exports = {
     'linebreak-style': 'off',
     'no-unused-expressions': 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+      ],
+      plugins: ['jest'],
+      env: {
+        jest: true,
+      },
+      // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+      ...require('eslint-plugin-jest').configs.recommended,
+    },
+  ],
 };
